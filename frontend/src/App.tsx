093f9f3ea7d8d0623b4abdaabb524cd700e31bdd
Igor Souza <igor.castro@sysmap.com.br>
2025-04-26 11:40:35 -0300

    implements epic games store integration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,47 +1,52 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import axios from "./lib/axios";
 
 interface Game {
-  id: string;
-  name: string;
-  short_description: string;
-  is_free: boolean;
-  required_age: string;
+  title: string;
   url: string;
-  header_image: string;
-  platforms: Platforms;
-  price_overview?: PriceOverview;
-}
-
-interface Platforms {
-  windows: boolean;
-  mac: boolean;
-  linux: boolean;
-}
-
-interface PriceOverview {
-  currency: string;
-  discount_percent: number;
-  final: number;
-  final_formatted: string;
-  initial: number;
-  initial_formatted: string;
+  image: string;
+  initialPrice: number;
+  discountPrice: number;
+  discountPercent: number;
 }
 
 function App() {
-  const [games, setGames] = useState<Game[]>([]);
+  const [games, setGames] = useState<Game[]>();
   const [search, setSearch] = useState<string>("");
+  const [gameStore, setGameStore] = useState<string>("steam");
+  const [loading, setLoading] = useState<boolean>(false);
+  const [searchedGameStore, setSearchedGameStore] = useState<
+    "Steam" | "Epic Games Store"
+  >("Steam");
 
-  async function getGames() {
+  async function getGames(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
     if (search.trim() === "") return;
 
-    const response = await axios.get(`/games?gameName=${search}`);
+    setLoading(true);
+    const response = await axios.get(`/games/${gameStore}?gameName=${search}`);
     setGames(response.data);
+    setSearchedGameStore(gameStore === "steam" ? "Steam" : "Epic Games Store");
+    setLoading(false);
+  }
+
+  function formatPrice(price: number): string {
+    const priceParts = price.toString().split(".");
+
+    if (priceParts.length === 1) {
+      return price + ",00";
+    }
+
+    if (priceParts[1].length === 1) priceParts[1] += "0";
+    return priceParts.join(",");
   }
 
   return (
     <div className="text-white">
-      <div className="flex justify-center items-center mt-10 text-xl">
+      <form
+        className="flex justify-center items-center mt-10 text-xl"
+        onSubmit={getGames}
+      >
         <label htmlFor="gameName">Nome do Jogo: </label>
         <input
           id="gameName"
@@ -49,58 +54,89 @@ function App() {
           className="border mx-3 p-2 rounded-md min-w-lg"
           onChange={(event) => setSearch(event.target.value)}
         />
+
+        <select
+          disabled={loading}
+          className={`mx-4 border py-2 px-1 rounded-md ${
+            loading && "opacity-30"
+          }`}
+          onChange={(event) => setGameStore(event.target.value)}
+        >
+          <option value="steam" className="text-black">
+            Steam
+          </option>
+          <option value="epic" className="text-black">
+            Epic Games Store
+          </option>
+        </select>
+
         <button
-          className="px-3 py-2 rounded-md cursor-pointer bg-white text-black hover:bg-gray-300"
-          onClick={getGames}
+          disabled={loading}
+          className={`px-3 py-2 rounded-md bg-white text-black ${
+            loading ? "opacity-30" : "cursor-pointer hover:bg-gray-300"
+          }`}
         >
           Pesquisar
         </button>
-      </div>
+      </form>
 
-      <div className="flex flex-col items-center">
-        <h1 className="text-2xl my-8">Resultados</h1>
+      <div className="flex flex-col gap-y-5 items-center">
+        <div className="text-center max-w-1/2">
+          {games && (
+            <h1 className="text-2xl mt-8 mb-2">
+              {games.length > 0 ? "Resultados" : `Sem resultados.`}
+            </h1>
+          )}
 
-        {games?.map((game) => (
-          <div
-            className="flex justify-between items-center mb-5 min-w-1/2 p-2 bg-gray-800"
-            key={game.id}
-          >
-            <img src={game.header_image} alt="Game Image" />
+          {searchedGameStore === "Epic Games Store" && (
+            <p>
+              Observação: devido a problemas com a API da Epic Games, os links
+              para os jogos na Epic Games Store podem estar incorretos.{" "}
+            </p>
+          )}
+        </div>
 
-            <div className="flex flex-col items-center justify-center ml-5 text-center w-full max-w-[300px]">
-              <h2 className="text-xl mb-2">{game.name}</h2>
+        {games?.map((game) => {
+          const gameInitialPrice = formatPrice(game.initialPrice);
+          const gameDiscountPrice = formatPrice(game.discountPrice);
 
-              <div className="flex flex-col">
-                {game.is_free || !game.price_overview ? (
-                  <p>Gratuito</p>
-                ) : (
-                  <>
-                    <p>
-                      Preço Original:{" "}
-                      {game.price_overview?.initial_formatted ||
-                        game.price_overview?.final_formatted}
-                    </p>
+          return (
+            <div
+              className="flex justify-between items-center px-4 py-6 bg-gray-800 w-full max-w-[640px]"
+              key={game.title}
+            >
+              <img src={game.image} alt="Game Image" className="w-[300px]" />
 
-                    <p>Preço Atual: {game.price_overview?.final_formatted}</p>
+              <div className="flex flex-col items-center justify-center ml-5 text-center w-full max-w-[300px]">
+                <h2 className="text-xl mb-2 font-bold">{game.title}</h2>
 
-                    {game.price_overview.discount_percent > 0 && (
-                      <p>Desconto: {game.price_overview.discount_percent}%</p>
-                    )}
-                  </>
-                )}
+                <div className="flex flex-col">
+                  {game.discountPrice > 0 ? (
+                    <>
+                      <p>Preço Original: R$ {gameInitialPrice}</p>
+                      <p>Preço Atual: R$ {gameDiscountPrice}</p>
+
+                      {game.discountPercent > 0 && (
+                        <p>Desconto: {game.discountPercent}%</p>
+                      )}
+                    </>
+                  ) : (
+                    <p>Gratuito</p>
+                  )}
+                </div>
+
+                <a
+                  href={game.url}
+                  target="_blank"
+                  rel="external"
+                  className="mt-2 break-words text-blue-500 max-w-[80%]"
+                >
+                  Clique aqui para acessar o jogo na {searchedGameStore}
+                </a>
               </div>
-
-              <a
-                href={game.url}
-                target="_blank"
-                rel="external"
-                className="mt-2 break-words text-blue-500 max-w-[80%]"
-              >
-                Clique aqui para acessar o jogo na Steam
-              </a>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </div>
   );
